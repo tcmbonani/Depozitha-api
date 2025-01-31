cb140a1bf4f29821cf0fc1a5d2621889528cc781
mbonanic <tcmbonani@gmail.com>
2025-01-31 11:33:46 +0200

    api

--- api/genvoucher.js
+++ api/genvoucher.js
@@ -1,25 +1,27 @@
+// Example: /api/genvoucher.js (backend code)
 import express from 'express';
 import admin from 'firebase-admin';
 import cors from 'cors';
 
-// Initialize Firebase Admin SDK
+// Initialize Firebase Admin SDK using environment variables
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-        "type": "service_account",
-        "project_id": "depozitha-merchants",
-        "private_key_id": "1cdd9eb646bafd92d113325f0a6e4abaf40f5a42",
-        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC3S3YrLWdhJPr4\nkCxFKgryQ5pX1AXx3jltEYqOLeSC9QQPQ/bNWT2NvfcMNIY0hTfgqW3vvn826hke\nmoBkoOJ5KxOQYXKhzy5lKqmHRVsLfcL/eJLc+m9cPSq2CDelq6/8CsQ9voUZU4vt\nzPr2zsqLa6piNZHznN0jBnlZ2FHl16G0BmT4TtKPQUft8la+zzI4I3RALrC9Jm8O\nGhQgUXsPUcE8tB/yZcBO7xlNwmQNpfsyIA5IVl866e8+y/tqOjdwMCTbs3VRYVKE\n8WEdh8vqp0xxcG5JLq0gW3UWrLUQu3nV0nbZZtHK50E3ULJJmj4tS9wvIIAK8vTL\nH8aPa9j1AgMBAAECggEACl5VLKuYl7zzCnkDXBU9AcfTtQisHmq69mHB4/mSHdtU\n07U9Qo/8BKbckP7uoY1wo/+JlcCBqz5SVfIY9bhJH2ARrv7oMGGLJxNi/CEu/Ycr\nbu6n5iKvhnj6T8pGtZMw9RG52KFMn6pjdbv87VW0zO61+HC91RlIRBkzZ6LuPRFd\nU//Q4kBAzIxlvzRhDm4cL+C1oiC3s4l/bEjznhz0+B1z7/I3TlXiJ/j/AmJQPhYi\nS0yK4ZObQWqMh94WRS8salakEKNwfHB/+eoZufKHrWfBV7EzhAHj65XzqTQ/0PbB\ngXsKsFelHmExdei9lBcJymReIexADPv/ru6j5ll/uQKBgQDc4eorGyLbBWQ7jNkg\nWoSV6WmRBR1TrF9i72GpnfTRuCLYe44FX7pCkOjE3eSFvP7pY8XChh5N/8c9ansG\nl5MWDuil4CpQBCZmXupUyuvPUwb+7qtxJPPi891m9e5S+NddYreFJ6tfbqYvmg4l\nQbbkk7ojC3P90QJTeAn0AlmIDQKBgQDUb7LSx3ZMzJuVB6XSmtUf4/nZF07nABZX\nWhkdj7xqkMv8Y9ixunL4nciHIXxJWwCAqNDiLy/YHzMQZ4fuVJB5TVGefILEUukr\nIbBpCp7GVnGoMw+fKXv23t4OvRZ+mQ0mA7fSpTFx+pcnztx9eIqbU6QorQNLQrBq\nbnXEZU2yiQKBgCqf8heFchGAFdqkEJZ8wU9IrtWMfh9SiUnHVjpsP/1kolzSYxLI\naWJFIpYzF9FzKmIRne0vApQcwu8oKDuaboj4MY8pFiFA28QvltrsGdt1c0ko966q\n6eGnG9etp+MdIDpmPXIVuK1+5dUTzp0D9G5hz67JnAWkGuXOqWUMOwjVAoGAS6zm\ntPNdDwqsObCZv6ZsWYnqzQzijKOZUdDEtaUoZ4XhRK2E2fsjTbFXZ9vj3HXQfgSl\nNI2+q2yh0iwvXhU9yhhjBOQx6SHn8fd0Ulb4LItKvbJC1F4AzPajf/iX9M0sw+f7\nKcyzCnlMM2AbZ47zQVXQWrUXQU4mbiOfTDESNpECgYEAuV4xwCfjVm17lLxhl0K/\nsKp2S1gpF6kNnTezhQswhNSYibEMhDrnpMqE19aa0UMJq7CWGyzal7e23AnHG/NC\nNqAqeJNORTH30NnhpqQAolQYGJqd0bNY+XT+gWVaYCeRqLYDGA0eRKVYtMMFt0+r\npg+OWfbEbK7gilP+43qwXc0=\n-----END PRIVATE KEY-----\n",
-        "client_email": "firebase-adminsdk-ayhu9@depozitha-merchants.iam.gserviceaccount.com",
-        "client_id": "103271628589851448746",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ayhu9%40depozitha-merchants.iam.gserviceaccount.com",
-        "universe_domain": "googleapis.com"
+      type: 'service_account',
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Fix line breaks
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_CLIENT_ID,
+      auth_uri: process.env.FIREBASE_AUTH_URI,
+      token_uri: process.env.FIREBASE_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+      client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
     }),
   });
 }
+
+// API logic here...
 
 // Create an Express app (even though it's running as serverless, we can use express)
 const app = express();
